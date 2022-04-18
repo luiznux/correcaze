0d3eb68a5217463d11a823b851379f483b0fa8de
raphaelabrantes <antenabr2@gmail.com>
2022-04-18 20:31:45 -0300

    Adicionando main loop basico

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import pygame
+HEIGHT = 640
+WIDTH = 480
+
+pygame.init()
+clock = pygame.time.Clock()
+window = pygame.display.set_mode((HEIGHT, WIDTH))
+
 if __name__ == '__main__':
-    print('first commit')
+    while True:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.WINDOWCLOSE:
+                exit()
