c30e420815448c41e484bd2d889d69d1785dd91b
Edison Neto <edison.aguiar.neto@gmail.com>
2022-05-01 13:36:47 -0300

    Adiciona a funcionalidade de cada botão
    - Botão de iniciar o jogo renderiza o jogo
    - Botão de créditos fecha o jogo(enquanto essa tela não é desenvolvida)
    - Botão de sair fecha o jogo
    
    Tem um pequeno bug na renderização do jogo, o menu não é apagado do
    fundo.

--- classes/menu.py
+++ classes/menu.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import pygame
 
 from contants import *
@@ -5,8 +6,45 @@ from contants import *
 
 class Menu:
     def __init__(self, surface: pygame.surface.Surface):
-        self._surface = surface
-        self._button_font = pygame.font.SysFont("Monaco", 40)
+        self.__surface = surface
+        self.__button_font = pygame.font.SysFont("Monaco", 40)
+        self.__start_button = None
+        self.__credits_button = None
+        self.__quit_button = None
+
+    def clicked_on_start_game(self, event: pygame.event.Event) -> bool:
+        if self.__start_button is None:
+            return False
+        return self.__was_click_over_button(event, self.__start_button)
+
+    def clicked_on_credits(self, event: pygame.event.Event) -> bool:
+        if self.__credits_button is None:
+            return False
+        return self.__was_click_over_button(event, self.__credits_button)
+
+    def clicked_on_quit(self, event: pygame.event.Event) -> bool:
+        if self.__quit_button is None:
+            return False
+        return self.__was_click_over_button(event, self.__quit_button)
+
+    def __was_click_over_button(
+        self, event: pygame.event.Event, button: pygame.rect.Rect
+    ):
+        if event.type != pygame.MOUSEBUTTONUP:
+            return False
+
+        mouse_position = pygame.mouse.get_pos()
+        mouse_x = mouse_position[0]
+        mouse_y = mouse_position[1]
+        button_x_range = (button.x, button.x + button.width)
+        button_y_range = (button.y, button.y + button.height)
+
+        if (mouse_x >= button_x_range[0] and mouse_x <= button_x_range[1]) and (
+            mouse_y >= button_y_range[0] and mouse_y <= button_y_range[1]
+        ):
+            return True
+
+        return False
 
     def render(self):
         title_surface = self.__render_main_title()
@@ -19,32 +57,40 @@ class Menu:
         title_surface = title_font.render("Corre Cazé", True, (0, 0, 0))
         # Define o X como o meio da tela
         title_surface_x = (WIDTH / 2) - (title_surface.get_width() / 2)
-        return self._surface.blit(title_surface, (title_surface_x, 100))
+        return self.__surface.blit(title_surface, (title_surface_x, 100))
 
     def __render_start_game_button(self, title_surface: pygame.rect.Rect):
-        self.__render_centralized_button(title_surface, "Iniciar Jogo", 150)
+        self.__start_button = self.__render_centralized_button(
+            title_surface, "Iniciar Jogo", 150
+        )
 
     def __render_credits_button(self, title_surface: pygame.rect.Rect):
-        self.__render_centralized_button(title_surface, "Créditos", 300)
+        self.__credits_button = self.__render_centralized_button(
+            title_surface, "Créditos", 300
+        )
 
     def __render_quit_button(self, title_surface: pygame.rect.Rect):
-        self.__render_centralized_button(title_surface, "Sair", 450)
+        self.__quit_button = self.__render_centralized_button(
+            title_surface, "Sair", 450
+        )
 
     def __render_centralized_button(
         self, title_surface: pygame.rect.Rect, text: str, y: int
-    ):
+    ) -> pygame.rect.Rect:
         button = pygame.Rect((title_surface.x, y, title_surface.width, 100))
-        pygame.draw.rect(self._surface, (255, 0, 0), button)
-        text_surface = self._button_font.render(text, True, (0, 0, 0))
+        pygame.draw.rect(self.__surface, (255, 0, 0), button)
+        text_surface = self.__button_font.render(text, True, (0, 0, 0))
 
         width_margin, height_margin = self.__calculate_button_margins(
             button, text_surface
         )
 
-        self._surface.blit(
+        self.__surface.blit(
             text_surface,
             (button.x + width_margin, button.y + height_margin),
         )
+
+        return button
 
     def __calculate_button_margins(
         self, button: pygame.Rect, text: pygame.surface.Surface
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-
 import pygame
+
+from enum import Enum
 
 from classes.caze import Caze
 from classes.menu import Menu
@@ -11,47 +11,74 @@ clock = pygame.time.Clock()
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 bg = pygame.Surface((WIDTH, HEIGHT))
 bg.fill((255, 255, 255))
-image = pygame.image.load('assets/caze_placeholder.png').convert_alpha()
+image = pygame.image.load("assets/caze_placeholder.png").convert_alpha()
 
 pygame.font.init()
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
 
-
-def initialize_lanes(surface):
+def initialize_lanes(surface: pygame.surface.Surface):
     lanes_width = (surface.get_width() - 200) / 3
     lanes_height = surface.get_height() - 100
-    return (pygame.Rect(100, 200, lanes_width, lanes_height),
-            pygame.Rect(100 + lanes_width, 200, lanes_width, lanes_height),
-            pygame.Rect(100 + lanes_width * 2 - 1, 200, lanes_width, lanes_height))
+    return (
+        pygame.Rect(100, 200, lanes_width, lanes_height),
+        pygame.Rect(100 + lanes_width, 200, lanes_width, lanes_height),
+        pygame.Rect(100 + lanes_width * 2 - 1, 200, lanes_width, lanes_height),
+    )
 
 
-def prepare_lanes(surface):
+def prepare_lanes(surface: pygame.surface.Surface):
     lane1, lane2, lane3 = initialize_lanes(surface)
     pygame.draw.rect(surface, color=(220, 20, 10), rect=lane1)
     pygame.draw.rect(surface, color=(180, 60, 160), rect=lane2)
     pygame.draw.rect(surface, color=(140, 80, 250), rect=lane3)
 
 
-# prepare_lanes(bg)
-# cazezinho = Caze(bg, image)
-menu = Menu(bg)
+def render_game():
+    prepare_lanes(bg)
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            cazezinho.change_lane("left")
+        if event.key == pygame.K_RIGHT:
+            cazezinho.change_lane("right")
+    bg.blit(cazezinho.image, (lanes[cazezinho.lane], cazezinho.image.get_height()))
 
-if __name__ == '__main__':
+
+menu = Menu(bg)
+cazezinho = Caze(bg, image)
+
+
+class GameState(Enum):
+    Menu = 1
+    Playing = 2
+    Paused = 3
+    Credits = 4
+
+
+state = GameState.Menu
+
+if __name__ == "__main__":
     while True:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.WINDOWCLOSE:
+                pygame.quit()
                 exit()
 
-            menu.render()
-            #if event.type == pygame.KEYDOWN:
-            #    if event.key == pygame.K_LEFT:
-            #        cazezinho.change_lane('left')
-            #    if event.key == pygame.K_RIGHT:
-            #        cazezinho.change_lane('right')
-        window.blit(bg, (0, 0))
-        #window.blit(cazezinho.image, (lanes[cazezinho.lane], cazezinho.image.get_height()))
-        pygame.display.flip()
+            if state == GameState.Menu:
+                menu.render()
+            elif state == GameState.Playing:
+                render_game()
+            elif state == GameState.Credits:
+                # TODO
+                exit()
 
+            if menu.clicked_on_start_game(event):
+                state = GameState.Playing
+            elif menu.clicked_on_credits(event):
+                state = GameState.Credits
+            elif menu.clicked_on_quit(event):
+                pygame.quit()
+                exit()
+
+        window.blit(bg, (0, 0))
+        pygame.display.flip()
