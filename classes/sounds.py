58ce2b37ddf44db372c6ea120385de46fa8a28de
luiznux <luiztagli@hotmail.com>
2022-05-21 19:44:26 -0300

    evita  a sobreposição de efeitos sonoros no jogo

--- classes/sounds.py
+++ classes/sounds.py
@@ -2,10 +2,12 @@ from pygame import mixer
 
 
 class Sounds:
+
     def __init__(self) -> None:
-        self.__is_playing_bg_music = False
-        self.__background_volume = 0.10
-        self.__effects_volume = 0.095
+        self.__is_playing_background_music = False
+        self.__is_playing_sound_effect = False
+        self.__background_volume = 0.08
+        self.__effects_volume = 0.10
         mixer.music.set_volume(self.__background_volume)
 
     sound_effects_map = {
@@ -17,16 +19,22 @@ class Sounds:
     background_music_map = {
         "hino-do-vasco": "assets/background-music/hino-do-vasco.mp3",
         "hino-do-flamengo": "assets/background-music/hino-do-flamengo.mp3",
-        "hino-do-corinthians": "assets/background-music/hino-do-corinthians.mp3",
+        "hino-do-corinthians":
+        "assets/background-music/hino-do-corinthians.mp3",
     }
 
     def play_sound_effect(self, sound_effect_name: str):
         sound = mixer.Sound(self.sound_effects_map.get(sound_effect_name))
         sound.set_volume(self.__effects_volume)
-        return sound.play()
+        if self.__is_playing_sound_effect:
+            sound.fadeout(1000)
+            self.__is_playing_sound_effect = False
+        else:
+            sound.play()
+            self.__is_playing_sound_effect = True
 
     def play_background_music(self, music_name: str):
-        if self.__is_playing_bg_music:
+        if self.__is_playing_background_music:
             return
 
         # Verifica se alguma musica ja esta rondando em background
@@ -35,4 +43,4 @@ class Sounds:
 
         mixer.music.load(self.background_music_map[music_name])
         mixer.music.play(-1)
-        self.__is_playing_bg_music = True
+        self.__is_playing_background_music = True
