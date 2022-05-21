c77d86190610ccb933bf2c7d78487dbca9370228
luiznux <luiztagli@hotmail.com>
2022-05-21 19:05:51 -0300

    refatora a classe sounds
    - adiciona novos atributos no construtor para controle de volume e flag da
        musica de fundo
    - add verifcação da flag antes de colocar a musica para rodar

--- classes/sounds.py
+++ classes/sounds.py
@@ -3,29 +3,38 @@ from pygame import mixer
 
 class Sounds:
 
+    def __init__(self) -> None:
+        self.__is_playing_bg_music = False
+        self.__background_volume = 0.10
+        self.__effects_volume = 0.095
+        mixer.music.set_volume(self.__background_volume)
+
     sound_effects_map = {
         "dentroo": "assets/sound-effects/dentrooo.mp3",
         "come-carne": "assets/sound-effects/ele-ta-dilacerando-a-carne.mp3",
-        "meu-deus": "assets/sound-effects/meu-deus-que-isso.mp3"
+        "meu-deus": "assets/sound-effects/meu-deus-que-isso.mp3",
     }
 
     background_music_map = {
         "hino-do-vasco": "assets/background-music/hino-do-vasco.mp3",
         "hino-do-flamengo": "assets/background-music/hino-do-flamengo.mp3",
         "hino-do-corinthians":
-        "assets/background-music/hino-do-corinthians.mp3"
+        "assets/background-music/hino-do-corinthians.mp3",
     }
 
     def play_sound_effect(self, sound_effect_name: str):
         sound = mixer.Sound(self.sound_effects_map.get(sound_effect_name))
-        return sound.play
+        sound.set_volume(self.__effects_volume)
+        return sound.play()
 
     def play_background_music(self, music_name: str):
+        if self.__is_playing_bg_music:
+            return
+
         # Verifica se alguma musica ja esta rondando em background
         if mixer.music.get_busy():
             mixer.music.unload()
-            mixer.music.load(self.background_music_map[music_name])
-            return mixer.music.play(-1)
 
         mixer.music.load(self.background_music_map[music_name])
-        return mixer.music.play(-1)
+        mixer.music.play(-1)
+        self.__is_playing_bg_music = True
